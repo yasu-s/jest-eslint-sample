fdf4e5d7717b6caddf54b08fb9bb42dfa06ef7a0
Yasuyuki Saito <ya_saito@uluru.jp>
2021-09-04 18:21:19 +0900

    ルール修正

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # jest-eslint-sample
+
+## 概要
+
+- jest + eslint + eslint-plugin-jest のサンプルです。
+
+## 環境
+
+- jest
+- eslint
+- eslint-plugin-jest
+- eslint-plugin-jest-formatting
+
+## URL
+
+- https://jestjs.io/ja/
+- https://eslint.org/
+- https://www.npmjs.com/package/eslint-plugin-jest
+- https://www.npmjs.com/package/eslint-plugin-jest-formatting
--- test/sample-test.js
+++ test/sample-test.js
@@ -9,4 +9,28 @@ describe('test', () => {
     // verify
     expect(actual).toBeTruthy();
   });
+
+  describe('aaa', () => {
+    test('includes3', () => {
+      // setup
+      const items = ['1', '2'];
+
+      // exercise
+      const actual = items.includes('2');
+
+      // verify
+      expect(actual).toBeTruthy();
+    });
+  });
+
+  test('includes2', () => {
+    // setup
+    const items = ['1', '2'];
+
+    // exercise
+    const actual = items.includes('2');
+
+    // verify
+    expect(actual).toBeTruthy();
+  });
 });
