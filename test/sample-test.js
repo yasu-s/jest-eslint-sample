d5cf0be1fc5552365fc9697bd01926a8bd5fb72b
Yasuyuki Saito <ya_saito@uluru.jp>
2021-09-05 16:22:19 +0900

    table形式テスト

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,4 +1,6 @@
 describe('test', () => {
+  const testItems = ['1', '2'];
+
   test('includes', () => {
     // setup
     const items = ['1', '2'];
@@ -10,12 +12,25 @@ describe('test', () => {
     expect(actual).toBeTruthy();
   });
 
-  describe('aaa', () => {
+  describe('Parameterized test', () => {
     test.each([
-      { items: ['1', '2'], target: '1', expected: true },
+      { items: testItems,  target: '1', expected: true },
       { items: ['1', '2'], target: '2', expected: true },
       { items: ['1', '2'], target: '3', expected: false },
-    ])('Parameterized test - $target', ({ items, target, expected }) => {
+    ])('object - $target', ({ items, target, expected }) => {
+      // exercise
+      const actual = items.includes(target);
+
+      // verify
+      expect(actual).toBe(expected);
+    });
+
+    test.each`
+      items         | target   | expected
+      ${testItems}  | ${'1'}   | ${true}
+      ${testItems}  | ${'2'}   | ${true}
+      ${['1', '2']} | ${'3'}   | ${false}
+    `('table - $target', ({ items, target, expected })=> {
       // exercise
       const actual = items.includes(target);
 
